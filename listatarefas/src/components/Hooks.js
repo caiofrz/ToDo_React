aefc8a7250e130d7ef6522012415e693520d9cc3
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-03-15 16:31:18 -0300

    brincando com react

---
+++ listatarefas/src/components/Hooks.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+
+const Hooks = () => {
+    let idade = 20;
+
+    const [novaIdadde, setIdade] = useState(idade);
+
+    const changeAge = () => {
+         idade ++;
+         console.log(idade);
+    }
+
+    function newAge() {
+        setIdade(idade++)
+    }
+    return(
+        <div>
+            <span>Idade: {idade}</span><br></br>
+            <button onClick={changeAge}>Aumentar idade</button>
+            <hr />
+            <span>Idade: {novaIdadde}</span><br></br>
+            <button onClick={newAge}>Aumentar idade com state</button>
+        </div>
+    )
+}
+
+export default Hooks;
